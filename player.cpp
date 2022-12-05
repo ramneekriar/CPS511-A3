ef135f3fad17e512b438d3e3b4d53a0bd625e962
rkriar <ramneekriar@gmail.com>
2022-12-04 22:50:27 -0500

    Fixed cannon mesh, it is drawn properly

--- player.cpp
+++ player.cpp
@@ -40,18 +40,11 @@ void Player::drawPlayerCannon()
     glTranslatef(0, 0, 28.0);
 
     glPushMatrix();
-//     Creating cylinder object for cannon
-//    GLUquadricObj *myCannon;
-//    myCannon = gluNewQuadric();
-//    gluQuadricDrawStyle(myCannon, GLU_SMOOTH);
-//    glRotatef(playerCannonAngleX, 1, 0, 0); //x-axis rotate
-//    glRotatef(playerCannonAngleY, 0, 0, 1); //y-axis rotate
     
     glRotatef(playerCannonAngleX, 1, 0, 0); //x-axis rotate
     glRotatef(playerCannonAngleY, 0, 1, 0); //y-axis rotate
-    glRotatef(90, 1, 0, 0);
-//    gluCylinder(myCannon, playerCannonRadius, playerCannonRadius, playerCannonHeight, 100, 100);
-//    glPushMatrix();
+    glRotatef(270, 1, 0, 0);
+
     createMesh();
     
     glPopMatrix();
@@ -93,66 +86,25 @@ void Player::mouseMotion(int x, int y)
     
 }
 
-//void Player::readMesh()
-//{
-//    FILE *fp;
-//
-//    fp = fopen("mesh.obj", "r");
-//
-//    float v1, v2, v3, n1, n2, n3;
-//    int i1, i2, i3, i4;
-//
-//    // Getting vertices values
-//    for(int i = 0; i < 1584; i+= 3){
-//        fscanf(fp, "v %f %f %f", &v1, &v2, &v3);
-//        vertices[i] = v1;
-//        vertices[i + 1] = v2;
-//        vertices[i + 2] = v3;
-//    }
-//
-//    // Getting normals values
-//    for(int i = 0; i < 1584; i+= 3){
-//        fscanf(fp, "vn %f %f %f", &n1, &n2, &n3);
-//        normals[i] = n1;
-//        normals[i + 1] = n2;
-//        normals[i + 2] = n3;
-//
-//        printf("%f", n1);
-//        printf("%f", n2);
-//        printf("%f", n3);
-//    }
-//
-//    // Getting index values
-//    for(int i = 0; i < 2048; i+= 4){
-//        fscanf(fp, "f %d %d %d %d", &i1, &i2, &i3, &i4);
-//        indices[i] = i1;
-//        indices[i + 1] = i2;
-//        indices[i + 2] = i3;
-//        indices[i + 3] = i4;
-//    }
-//    
-//    fclose(fp);
-//}
-
 void Player::createMesh()
 {
-    
-//    glPushMatrix();
+    glPushMatrix();
     for (int i = 0; i < 1584; i+= 12){
         glBegin(GL_QUADS);
-        
-        glNormal3f(normals[i], normals[i + 1], normals[i + 2]);
+
+        glNormal3f(normals[indices[i]], normals[indices[i] + 1], normals[indices[i] + 2]);
         glVertex3f(vertices[i], vertices[i + 1], vertices[i + 2]);
-        
-        glNormal3f(normals[i + 3], normals[i + 4], normals[i + 5]);
-        glVertex3f(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
-        
-        glNormal3f(normals[i + 6], normals[i + 7], normals[i + 8]);
-        glVertex3f(vertices[i + 6], vertices[i + 7], vertices[i + 8]);
-        
-        glNormal3f(normals[i + 9], normals[i + 10], normals[i + 11]);
-        glVertex3f(vertices[i + 9], vertices[i + 10], vertices[i + 11]);
-        
+
+        glNormal3f(normals[indices[i] + 3], normals[indices[i] + 4], normals[indices[i] + 5]);
+        glVertex3f(vertices[indices[i] + 3], vertices[indices[i] + 4], vertices[indices[i] + 5]);
+
+        glNormal3f(normals[indices[i] + 6], normals[indices[i] + 7], normals[indices[i] + 8]);
+        glVertex3f(vertices[indices[i] + 6], vertices[indices[i] + 7], vertices[indices[i] + 8]);
+
+        glNormal3f(normals[indices[i] + 9], normals[indices[i] + 10], normals[indices[i] + 11]);
+        glVertex3f(vertices[indices[i] + 9], vertices[indices[i] + 10], vertices[indices[i] + 11]);
+
         glEnd();
     }
+    glPopMatrix();
 }
